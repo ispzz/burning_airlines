ed0af5787c41ff10c90b4c9110ad478a8e448a10
Carmina <cvdsoto@gmail.com>
2020-03-31 22:17:57 +1100

    revised cors config- mina

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins '*'
     resource '*',
       :headers => :any,
-      :methods => %i(get post put patch delete options head)
-      # %i is array of symbols
+      :methods => %i( get post put patch delete options head )
   end
 end
