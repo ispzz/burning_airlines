ce9a39ad5ece8aa0b968d683cf2cb7738ad42ad3
doubleInc <aalavi@protonmail.com>
2020-04-01 10:40:10 +1100

    Seed data for 2 reservations - Ali

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Reservation.create([
+    seat: "1A",
+    user_id: 1,
+    flight_id: 1
+  ])
+
+  Reservation.create([
+    seat: "2B",
+    user_id: 2,
+    flight_id: 1
+  ])
