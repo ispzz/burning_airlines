818c21e3e300086cc7e58ac8f087ef6abf873913
Carmina <cvdsoto@gmail.com>
2020-04-02 11:53:13 +1100

    revised seed file- mina

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,91 @@
 Reservation.destroy_all
 
 Reservation.create([
-    seat: "1A",
-    user_id: 1,
-    flight_id: 1
+  seat: "1A",
+  user_id: 1,
+  flight_id: 1
   ])
 
-  Reservation.create([
-    seat: "2B",
-    user_id: 2,
-    flight_id: 1
+Reservation.create([
+  seat: "2B",
+  user_id: 2,
+  flight_id: 1
+  ])
+
+Reservation.create([
+  seat: "3D",
+  user_id: 5,
+  flight_id: 1
+  ])
+
+Reservation.create([
+  seat: "1D",
+  user_id: 4,
+  flight_id: 2
+  ])
+
+Reservation.create([
+  seat: "2C",
+  user_id: 3,
+  flight_id: 2
+  ])
+
+Reservation.create([
+  seat: "3B",
+  user_id: 7,
+  flight_id: 2
+  ])
+
+Reservation.create([
+  seat: "4D",
+  user_id: 2,
+  flight_id: 3
+  ])
+
+Reservation.create([
+  seat: "3C",
+  user_id: 1,
+  flight_id: 3
+  ])
+
+Reservation.create([
+  seat: "4C",
+  user_id: 6,
+  flight_id: 3
+  ])
+
+Reservation.create([
+  seat: "6D",
+  user_id: 1,
+  flight_id: 4
+  ])
+
+Reservation.create([
+  seat: "4C",
+  user_id: 6,
+  flight_id: 4
+  ])
+
+Reservation.create([
+  seat: "3A",
+  user_id: 2,
+  flight_id: 4
+  ])
+
+Reservation.create([
+  seat: "3B",
+  user_id: 3,
+  flight_id: 4
+  ])
+
+Reservation.create([
+  seat: "9C",
+  user_id: 4,
+  flight_id: 4
+  ])
+
+Reservation.create([
+  seat: "10D",
+  user_id: 5,
+  flight_id: 4
   ])
